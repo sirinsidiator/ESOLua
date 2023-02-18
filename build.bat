d9bee07390fa5dda9126106502797d3ab37e9694
sirinsidiator <insidiator@cmos.at>
2023-02-18 22:51:47 +0100

    add prefix "eso" to executables names in build output

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 make mingw
+rd /S /Q output
 md output
 robocopy src output *.exe *.dll
 robocopy . output COPYRIGHT HISTORY INSTALL README README.md
-if ErrorLevel 8 (exit /B 1) else (exit /B 0)
+rename output\lua.exe esolua.exe
+rename output\luac.exe esoluac.exe
