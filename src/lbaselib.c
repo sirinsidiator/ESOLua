b67c0530358e2defcb444dcb43086d6c74a198a4
sirinsidiator <insidiator@cmos.at>
2023-02-18 23:09:20 +0100

    adjusted error message when setting protected meta tables to match the game

--- src/lbaselib.c
+++ src/lbaselib.c
@@ -107,7 +107,7 @@ static int luaB_setmetatable (lua_State *L) {
   luaL_argcheck(L, t == LUA_TNIL || t == LUA_TTABLE, 2,
                     "nil or table expected");
   if (luaL_getmetafield(L, 1, "__metatable"))
-    luaL_error(L, "cannot change a protected metatable");
+    luaL_error(L, "cannot change the metatable");
   lua_settop(L, 2);
   lua_setmetatable(L, 1);
   return 1;
