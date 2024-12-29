ca14bb08f40a8c04343fa17fd4cd2f80b4d7b035
sirinsidiator <insidiator@cmos.at>
2024-12-29 17:58:06 +0100

    changed LUA_VERSION to "EsoLua"

--- src/lua.h
+++ src/lua.h
@@ -16,8 +16,8 @@
 #include "luaconf.h"
 
 
-#define LUA_VERSION	"Lua 5.1"
-#define LUA_RELEASE	"Lua 5.1.5"
+#define LUA_VERSION	"ESOLua"
+#define LUA_RELEASE	"ESOLua (based on Lua 5.1.5)"
 #define LUA_VERSION_NUM	501
 #define LUA_COPYRIGHT	"Copyright (C) 1994-2012 Lua.org, PUC-Rio"
 #define LUA_AUTHORS 	"R. Ierusalimschy, L. H. de Figueiredo & W. Celes"
